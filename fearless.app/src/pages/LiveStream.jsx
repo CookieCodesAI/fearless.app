08b837c708c5bf0cb8e3541f8dce2ab2dad48079
CookieCodesAI <kdhriti15@gmail.com>
2025-12-05 21:40:40 -0500

    Added LiveStream.jsx and basic live streaming function

--- fearless.app/src/pages/LiveStream.jsx
+++ fearless.app/src/pages/LiveStream.jsx
@@ -1,12 +1,19 @@
 import React, {useState, useEffect, useRef} from 'react';
+import { useNavigate } from 'react-router-dom';
 import "../components/styles/LiveStream.css"
+import WebCam from 'react-webcam'
 
 export default function LiveStream(){
-    const likes = "https://tenor.com/bdILF.gif";
+    const navigate = useNavigate();
     return (
-        <div>
-            <div>Write Live Stream Here</div>
-            <img src = {likes} alt = "likes" className="likes"></img>
+        <div className = "stream-container">
+            <img src ="/public/live-stream.gif" alt = "likes" className='likes'></img>
+            <WebCam className = 'webcam' audio={false} width={500} height={700} videoConstraints={{
+                width: 520,
+                height: 430,
+                facingMode: "user"
+            }}/>
+            <button className="end-live" onClick={()=>navigate('/')} >End Live Stream</button>
         </div>
     )
 }
